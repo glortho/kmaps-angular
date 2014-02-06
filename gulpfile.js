f834233b5ddc267cd56342b6272e8bb44eb3097d
Jed Verity <jed@dataverse.me>
2014-02-05 21:15:09 -0500

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var compass = require('gulp-compass');
+
+gulp.task('compass', function() {
+
+});
+
+gulp.task('default', function() {
+
+});
